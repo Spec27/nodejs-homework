36e25ae2b49a99e1496f2e926fe27c2e9e1baceb
Spec27 <shkvara27@gmail.com>
2022-09-01 13:16:38 +0300

    finished homewor

---
+++ models/user.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+const { handleSchemaValidationErrors } = require("../helpers");
+
+const emailRegexp = /^[\w.]+@[\w]+.[\w]+$/;
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      match: emailRegexp,
+      unique: true,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      required: [true, "Password is required"],
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleSchemaValidationErrors);
+
+const registerSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+  repeat_password: Joi.ref("password"),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  schemas,
+};
